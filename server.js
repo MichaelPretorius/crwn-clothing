d212a3af4027e1cfe663572b9241a4aceb4f0da6
Michael Pretorius <michaelpretorius96@gmail.com>
2019-09-21 14:58:09 +0200

    Updating code to be PWA compliant, adding service worker, https enforcing, updating manifest

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -12,6 +13,7 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
@@ -36,6 +38,10 @@ app.post('/payment', (req, res) => {
             res.status(200).send({ success: stripeRes });
         }
     });
+});
+
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 });
 
 app.listen(port, error => {
